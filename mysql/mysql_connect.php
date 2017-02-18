d72894aae3608ced92227828f813545ca99119e5
Yu <yuwenbin.dj@gmail.com>
2017-02-18 17:10:04 +0800

    mysql_connect测试

---
+++ mysql/mysql_connect.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pascal
+ * Date: 2017/2/18
+ * Time: 下午5:04
+ */
+
+//连接数据库
+$conn = mysql_connect('localhost','root','root');
+if(!$conn){
+    die('数据库连接失败！'.mysql_error());
+}else {
+    echo '连接成功';
+}
+
+//选择数据库
+
+$select_db = mysql_select_db('myphp');
+
+if(!$select_db)){
+    die('数据库选择失败！'.mysql_error());
+}else{
+    echo '选择数据库失败';
+}
+
+mysql_query('set names utf-8');
